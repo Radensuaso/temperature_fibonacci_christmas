b6837e6d44a7eecaa169f60bcb9d07f7f5972565
André Sousa <andrels9283@gmail.com>
2023-01-07 14:15:53 +0000

    nth fibonacci number done

--- src/fib.rs
+++ src/fib.rs
@@ -1,3 +1,38 @@
 pub(crate) fn nth_fibonacci() {
-    println!("{}", 441);
+    let n: u128 = loop {
+        let mut n = String::new();
+
+        println!("Type the nth integer you want to know the correspondent fibonacci.");
+
+        std::io::stdin()
+            .read_line(&mut n)
+            .expect("Failed to read line");
+
+        let value: u128 = match n.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        break value;
+    };
+
+    let n = n;
+
+    println!("The {n} number in fibonacci sequence is {}.", fibonacci(n));
+}
+
+fn fibonacci(n: u128) -> u128 {
+    let mut a = 0;
+    let mut b = 1;
+    let mut c = 0;
+    let mut result = 0;
+
+    for _ in 0..n {
+        result = c;
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return result;
 }
